0d5eaa302af742f15b6710075f287cad88ad0d0d
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-28 23:45:31 +0500

    background styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
+import styled from "styled-components";
 import ShortenUrl from "./ShortenUrl";
+
+import Background from "./Background";
+import Content from "./Content";
+
+const Background2 = styled(Background)`
+  animation-direction: alternate-reverse;
+  animation-duration: 4s;
+`;
+
+const Background3 = styled(Background)`
+  animation-duration: 5s;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Short Urls</h1>
-        <h3>Shorten your links</h3>
-      </header>
-      <main>
-        <ShortenUrl></ShortenUrl>
-      </main>
+      <Background />
+      <Background2 />
+      <Background3 />
+      <Content>
+        <header>
+          <h1>Short Urls</h1>
+          <h3>Shorten your links</h3>
+        </header>
+        <main>
+          <ShortenUrl></ShortenUrl>
+        </main>
+      </Content>
     </div>
   );
 }
